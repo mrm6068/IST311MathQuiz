f3a836bdaf67b7d70ae1cbe0a181e20e45c25d78
Mike0_000 <Mike0_000@MikePC>
2017-11-14 19:43:35 -0500

    Comments

--- src/ist311mathquiz/MathQuizDriver.java
+++ src/ist311mathquiz/MathQuizDriver.java
@@ -6,12 +6,12 @@ public class MathQuizDriver
     public static void main(String[] args) 
     {
         boolean nextLevel = false;//Holds whether user will move on.
-        MathQuiz mathQuiz = new MathQuiz();
+        MathQuiz mathQuiz = new MathQuiz();//Represents this entire quiz.
          
         //Start level one.
         nextLevel = mathQuiz.levelOne();
         
-        
+        //If last level was passed...
         if(nextLevel)
         {
             nextLevel = mathQuiz.levelTwo();
