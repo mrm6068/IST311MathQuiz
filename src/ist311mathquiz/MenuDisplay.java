881b74593e7a2eb642305527aae5e37e284f9d21
Adrian <avg5074@gmail.com>
2017-12-06 14:12:14 -0500

    Added a simple display class for now that displays what this program does and asks for user name,age input.

---
+++ src/ist311mathquiz/MenuDisplay.java
@@ -0,0 +1,53 @@
+package ist311mathquiz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian Galecki
+ */
+public class MenuDisplay {
+
+    private String playerName;
+    private int playerAge;
+
+    
+    public String getName() {
+        return playerName.substring(0,1).toUpperCase() + playerName.substring(1);
+    }
+
+    public void setName(String name) {
+        this.playerName = name;
+    }
+
+    public int getAge() {
+        return playerAge;
+    }
+
+    public void setAge(int age) {
+        this.playerAge = age;
+    }
+    
+    public void DisplayIntro(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("--=-- Math Quiz --=--");
+        System.out.println("\nThere are 3 levels of math questions" +
+                " plus bonus levels if you complete each level with a score" +
+                " of higher than 400");
+        System.out.println("Enter player name: ");
+        playerName = input.nextLine();
+        setName(playerName);
+        System.out.println("Enter player age: ");
+        playerAge = input.nextInt();
+        setAge(playerAge);
+    }
+    public void UserInfo(){
+        System.out.println("Let's get started");
+        System.out.println("\n___PLAYER___");
+        System.out.println("| " + getName() + " | " + getAge() + " |" );
+    }
+    
+
+}
+
